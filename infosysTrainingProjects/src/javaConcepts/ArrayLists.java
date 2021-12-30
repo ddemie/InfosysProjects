6d7d746eb757b03b99342c3929eb1a8e9143a344
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:53:46 -0700

    Update ArrayLists.java

--- infosysTrainingProjects/src/javaConcepts/ArrayLists.java
+++ infosysTrainingProjects/src/javaConcepts/ArrayLists.java
@@ -1,5 +1,5 @@
 package javaConcepts;
-
+//Four elements are added to the list. When a new element is inserted at a specified position, the element currently at that position gets shifted to the right.
 public class ArrayLists {
   public static void main(String args[]) {
 		List<String> list = new ArrayList<String>();
