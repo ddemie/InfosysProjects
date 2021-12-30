9a3bec1a8629004df6c7c2b109ebc1d39039823e
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:23:26 -0700

    Update ForLoops.java

--- infosysTrainingProjects/src/javaConcepts/ForLoops.java
+++ infosysTrainingProjects/src/javaConcepts/ForLoops.java
@@ -2,9 +2,17 @@ package javaConcepts;
 
 public class ForLoops {
 
+	public static class Numbers {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int rows = 10;
+		for (int i = 1; i <= rows; ++i) {
+			for (int j = 1; j <= i; ++j) {
+				System.out.print(j + " "); 
+			}
+			System.out.println(""); 
+		}
 	}
+}
+
 
 }
