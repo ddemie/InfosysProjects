8b7cf70b8b23088044487bb5ca6315d5a61cdf9c
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:25:59 -0700

    Update MultiDementionalArrays.java

--- infosysTrainingProjects/src/javaConcepts/MultiDementionalArrays.java
+++ infosysTrainingProjects/src/javaConcepts/MultiDementionalArrays.java
@@ -1,10 +1,24 @@
 package javaConcepts;
 
 public class MultiDementionalArrays {
-
+ 
+public static class Tester {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[][] dayWiseTemperature = new int[][] { { 29, 21 }, { 24, 23 },
+				{ 26, 22 }, { 28, 23 }, { 29, 24 }, { 23, 20 }, { 29, 21 } };
+
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 2; j++) {
+				if (j == 0)
+					System.out.println("Max Temperature is "
+							+ dayWiseTemperature[i][j] + " on day " + i);
+				else
+					System.out.println("Min Temperature is "
+							+ dayWiseTemperature[i][j] + " on day " + i);
+			}
+		}
 	}
+}
 
 }
