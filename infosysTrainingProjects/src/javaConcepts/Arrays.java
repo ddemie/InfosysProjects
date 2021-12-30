9a40e093a11c7c2cec0874ad10668e1be7eaf45a
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:06:43 -0700

    Update Arrays.java

--- infosysTrainingProjects/src/javaConcepts/Arrays.java
+++ infosysTrainingProjects/src/javaConcepts/Arrays.java
@@ -1,5 +1,19 @@
 package javaConcepts;
 
 public class Arrays {
+   public static int calculateSumOfEvenNumbers(int[] numbers){
+        int sum = 0;
+        for(int nums : numbers){
+            if(nums % 2==0){
+                sum += nums;
+            }
+        }
+        return sum;
+    }
+    
+	public static void main(String[] args) {
+		int[] numbers = {68,79,86,99,23,2,41,100};
+		System.out.println("Sum of even numbers: " +calculateSumOfEvenNumbers(numbers));
+	}
 
 }
