e3bb0de5688d127b79f677d63e86954b9e741b03
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:27:18 -0700

    Update Strings.java

--- infosysTrainingProjects/src/javaConcepts/Strings.java
+++ infosysTrainingProjects/src/javaConcepts/Strings.java
@@ -1,10 +1,39 @@
 package javaConcepts;
 
 public class Strings {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public static class Tester{
+    public static boolean checkPalindrome(String str){
+		
+		char[] letters = new char[str.length()];
+		
+		int letterIndex = 0;
+    	
+    	int lettersIndex= 0;
+    	
+	   for(int i = str.length() - 1; i >= 0; i-- ) {
+		  letters[letterIndex] = str.charAt(i);
+		  letterIndex++;
+	   }
+		
+		String reverse = "";
+		for(int i = 0; i < str.length(); i++) {
+			reverse = reverse + letters[i];
+		}
+		
+		boolean palendrome = false;
+		if(reverse.equals(str)) {
+			palendrome = true;
+		}
+		
+        return palendrome;
+    }
+	
+	public static void main(String args[]){
+		String str = "radar";
+		if(checkPalindrome(str))
+			System.out.println("The string is a palindrome!");
+		else
+			System.out.println("The string is not a palindrome!");
 	}
-
+}
 }
