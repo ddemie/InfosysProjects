c2fe397a29ba9ec18aa602550eae271df51a8e5f
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:40:16 -0700

    Update MethodOverloading.java

--- infosysTrainingProjects/src/javaConcepts/MethodOverloading.java
+++ infosysTrainingProjects/src/javaConcepts/MethodOverloading.java
@@ -1,5 +1,35 @@
 package javaConcepts;
 
 public class MethodOverloading {
+  public static class Tester {
+
+	public static void main(String[] args) {
+
+
+
+		Customer regCust = new RegularCustomer();
+	
+		System.out.println("Final Bill : " + regCust.payBill(40.0)); 
+	}
+}
+
+public static class Customer {
+
+	public double payBill(double totalPrice) {
+		System.out.println("Final bill for the customer is calculated here");
+		return totalPrice;
+	}
+}
+
+public static class RegularCustomer extends Customer {
+
+	@Override
+	public double payBill(double totalPrice) {
+		System.out.println("Final bill for the regular customer is calculated here");
+		double priceAfterDiscount = totalPrice * (1 - (5f / 100));
+		return priceAfterDiscount;
+	}
+
+}
 
 }
