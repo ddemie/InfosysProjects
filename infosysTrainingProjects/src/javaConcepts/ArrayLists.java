aecd5453b60ff098bf56be4498423ddb7ff7061a
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:32:22 -0700

    Update ArrayLists.java

--- infosysTrainingProjects/src/javaConcepts/ArrayLists.java
+++ infosysTrainingProjects/src/javaConcepts/ArrayLists.java
@@ -1,5 +1,13 @@
 package javaConcepts;
 
 public class ArrayLists {
+  public static void main(String args[]) {
+		List<String> list = new ArrayList<String>();
+		list.add("Pizza");
+		list.add(0, "Burger");
+		list.add(0, "Sandwich");
+		list.add(0, "Noodles");
+		System.out.println(list.size());
+	}
 
 }
