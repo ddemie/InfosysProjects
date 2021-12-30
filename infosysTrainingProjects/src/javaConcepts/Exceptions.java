a2bcf40960be1551861322fab2786642e0ecb3da
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:33:29 -0700

    Update Exceptions.java

--- infosysTrainingProjects/src/javaConcepts/Exceptions.java
+++ infosysTrainingProjects/src/javaConcepts/Exceptions.java
@@ -1,5 +1,22 @@
 package javaConcepts;
 
 public class Exceptions {
+  public static void main(String[] args) {
+		try {
+			method1();
+			System.out.print("A");
+		} catch (Exception e) {
+			System.out.print("B");
+		} catch (Throwable e) {
+			System.out.print("C");
+		} finally {
+			System.out.print("D");
+		}
+		System.out.println("E");
+	}
+
+	public static void method1() {
+		throw new Error();
+	}
 
 }
