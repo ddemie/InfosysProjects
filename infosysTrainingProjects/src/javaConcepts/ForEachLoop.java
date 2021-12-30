32c378d1975fe9ae6274ddfc098cfda18348e2ee
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:20:15 -0700

    Update ForEachLoop.java

--- infosysTrainingProjects/src/javaConcepts/ForEachLoop.java
+++ infosysTrainingProjects/src/javaConcepts/ForEachLoop.java
@@ -1,10 +1,45 @@
 package javaConcepts;
 
 public class ForEachLoop {
+public static class Tester {
+    
+    public static double[] findDetails(double[] salary) {
+       double averageSalary = 0;
+       double lesser = 0;
+       double greater = 0;
+       for(double sal: salary) {
+    	   averageSalary += sal/salary.length;
+       }
+       
+       
+       for(double greaterThanSal: salary) {
+    	   if(greaterThanSal > averageSalary) {
+    		   greater++;
+    	   }
+       }
+       for(double lessThanSal : salary) {
+    	   if(lessThanSal < averageSalary) {
+    		   lesser++;
+    	   }
+       }
+       
+       
+       
+       double finalNumbers[] = {averageSalary, greater, lesser};
+        
+        return finalNumbers;     
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    }
+       
+    public static void main(String[] args) {
+        double[] salary = { 23500.0, 25080.0, 28760.0, 22340.0, 19890.0 };
+        double[] details = findDetails(salary);
+              
+        System.out.println("Average salary: "+ details[0]);
+        System.out.println("Number of salaries greater than the average salary: "+ details[1]);
+        System.out.println("Number of salaries lesser than the average salary: "+ details[2]);
+    }
+}
 
-	}
 
 }
