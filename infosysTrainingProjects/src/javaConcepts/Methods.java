4f0785b02bb1f86087af634a1ee34febe0d3f9a5
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:24:43 -0700

    Update Methods.java

--- infosysTrainingProjects/src/javaConcepts/Methods.java
+++ infosysTrainingProjects/src/javaConcepts/Methods.java
@@ -1,5 +1,23 @@
 package javaConcepts;
 
 public class Methods {
+  public static class Calculator {
+
+	public double averageMethod(double a, double b, double c){
+	    double addedNumbers =  a+b+c;
+	    double dividedNumber = 3.0;
+	    double finalMethod = addedNumbers/dividedNumber;
+	    return finalMethod;
+	}
+}
+
+public static class Tester {
+
+	public static void main(String args[]) {
+		Calculator calculator = new Calculator();
+		
+		System.out.println(calculator.averageMethod(10, 20, 30));
+	}
+}
 
 }
