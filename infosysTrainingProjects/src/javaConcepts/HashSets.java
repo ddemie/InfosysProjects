26410865e039d3152c16aceda3656e13b7a5dcfc
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:34:30 -0700

    Update HashSets.java

--- infosysTrainingProjects/src/javaConcepts/HashSets.java
+++ infosysTrainingProjects/src/javaConcepts/HashSets.java
@@ -1,5 +1,15 @@
 package javaConcepts;
 
 public class HashSets {
+  public static void main(String[] args) {
+		Set<Integer> set = new HashSet<Integer>();
+		set.add(20);
+		set.add(50);
+		set.add(20);
+		System.out.print(set.size() + " ");
+		set.remove(20);
+		System.out.print(set.size() + " ");
+		set.add(50);
+		System.out.prin
 
 }
