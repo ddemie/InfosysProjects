46d9b90f261b93cb555999ed3285b1ed6d6a0d3a
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 01:00:27 -0700

    Update Exceptions.java

--- infosysTrainingProjects/src/javaConcepts/Exceptions.java
+++ infosysTrainingProjects/src/javaConcepts/Exceptions.java
@@ -1,5 +1,13 @@
 package javaConcepts;
 
+/*
+
+Since, Error class is a child class of Throwable class, 
+the corresponding catch block of Throwable will handle the exception and C is displayed. 
+Then, finally block is executed and after that E is displayed.
+
+*/
+
 public class Exceptions {
   public static void main(String[] args) {
 		try {
