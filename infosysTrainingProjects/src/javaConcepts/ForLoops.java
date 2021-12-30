64ddb0c910fc1f98bbd55c0c59adab1b2de5074c
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 01:05:29 -0700

    Update ForLoops.java

--- infosysTrainingProjects/src/javaConcepts/ForLoops.java
+++ infosysTrainingProjects/src/javaConcepts/ForLoops.java
@@ -1,5 +1,26 @@
 package javaConcepts;
 
+/*
+
+A nested loop is a loop within another loop, an inner loop within the body of an outer one.
+
+Let us print the following pattern:
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+1 2 3 4 5 6 
+1 2 3 4 5 6 7 
+1 2 3 4 5 6 7 8 
+1 2 3 4 5 6 7 8 9 
+1 2 3 4 5 6 7 8 9 10 
+
+We can create the above pattern using the nested loop.
+
+*/
+
 public class ForLoops {
 
 	public static class Numbers {
