690468d421da05987d23252a39764a639e2ca231
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 01:11:22 -0700

    Update HashSets.java

--- infosysTrainingProjects/src/javaConcepts/HashSets.java
+++ infosysTrainingProjects/src/javaConcepts/HashSets.java
@@ -1,5 +1,21 @@
 package javaConcepts;
 
+/*
+
+Set can contain only unique elements and size() method is used to get the number of elements present in the HashSet.
+
+At line 6, element 20 is added. Size=1
+
+At line 7, element 50 is added. Size =2
+
+At line 8, again element 20 is added. Since the element is not unique, Size = 2
+
+At line 10, element 20 is removed. Size =1
+
+At line 12, element 50 is added. Since the element is not unique, Size=1
+
+*/
+
 public class HashSets {
   public static void main(String[] args) {
 		Set<Integer> set = new HashSet<Integer>();
@@ -10,6 +26,6 @@ public class HashSets {
 		set.remove(20);
 		System.out.print(set.size() + " ");
 		set.add(50);
-		System.out.prin
-
+		System.out.print(set.size() + " ");
+	}
 }
