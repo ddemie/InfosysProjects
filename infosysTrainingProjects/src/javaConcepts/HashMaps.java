4f78514c2091a799c5d8b645379b57aa989eb921
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 00:30:58 -0700

    Update HashMaps.java

--- infosysTrainingProjects/src/javaConcepts/HashMaps.java
+++ infosysTrainingProjects/src/javaConcepts/HashMaps.java
@@ -2,9 +2,15 @@ package javaConcepts;
 
 public class HashMaps {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String args[]) {
+	Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+	
+	hashMap.put(101, 6);
+	hashMap.put(102, 8);
+	hashMap.put(103, 10);
+	hashMap.put(102, 5);
+	
+	hashMap.replace(102, 8, 15);
+	System.out.println(hashMap);
+}
 }
