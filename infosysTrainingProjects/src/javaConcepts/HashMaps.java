f74ee54925bb8c5231639a075f0b7bd5c40b4697
Dallin De Mie <67065993+ddemie@users.noreply.github.com>
2021-12-30 19:03:34 -0700

    Update HashMaps.java

--- infosysTrainingProjects/src/javaConcepts/HashMaps.java
+++ infosysTrainingProjects/src/javaConcepts/HashMaps.java
@@ -1,5 +1,8 @@
 package javaConcepts;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /* There is no entry where key is 102 with the corresponding value as 8. The value 8 is replaced by 5 for the key 102. 
 Therefore the final output will be {101=6, 102=5, 103=10}*/
 
